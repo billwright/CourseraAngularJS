5a4a216bbc493605706aa6c68bd4b528a398accf
bill.w.wright <bill.w.wright@oracle.com>
2016-09-17 06:14:40 -0600

    Changing the color of the text and border based on count of items

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -11,10 +11,20 @@ function LunchCheckController($scope) {
 
   $scope.checkLunch = function() {
     var parts = $scope.lunchList.split(',');
-    if (parts.length > 3) {
+
+    // Only count non-item items. So '1,,2,,3' counts as just three items
+    var nonEmptyItems = 0;
+    for (let item of parts) {
+      if (item.trim().length > 0) {
+        nonEmptyItems++;
+      }
+    }
+    if (nonEmptyItems > 3) {
       $scope.message = 'Too much!';
+      $scope.messageColor = 'red';
     } else {
       $scope.message = 'Eat up!';
+      $scope.messageColor = 'green';
     }
   }
 }
